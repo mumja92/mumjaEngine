183aadaf18b0aa8d6ce3497efa557087885cd25c
mumja92 <mumja92@gmail.com>
2021-03-08 21:52:27 +0100

    Add main class
    Kotlin doesn't use class as an entry point to application, but it is
    necessary to have class with static main function to create artifacts,
    so class is added to main file.

--- src/main.kt
+++ src/main.kt
@@ -4,3 +4,11 @@ import com.mumja.tetris.Tetris
 fun main(){
     Tetris().playSwing()
 }
+
+class Main {
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            Tetris().playSwing()
+        }
+    }
+}
